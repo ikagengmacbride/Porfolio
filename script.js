17aee1a0674afe00c941568be8b4f8125b618246
ikagengmacbride <macbridephiri@gmail.com>
2024-04-08 15:18:37 +0200

    cv

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+// Dark Mode
+const body = document.querySelector("body"),
+toggleSwitch = document.getElementById("toggle-switch");
+toggleSwitch.addEventListener('click', () => {
+body.classList.toggle("dark");
+})      
+
+
 function toggleMenu() {
   const menu = document.querySelector(".menu-links");
   const icon = document.querySelector(".hamburger-icon");
